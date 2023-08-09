09d862a54dfc631ea6c73ceef6005e09a35e9e96
Eugene <exsoulck@gmail.com>
2023-08-08 20:43:59 -0400

    updates

--- case_1/explicit_wait/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from selenium import webdriver
-
-
-@pytest.fixture
-def driver():
-    d = webdriver.Firefox()
-    d.get("https://chercher.tech/practice/explicit-wait-sample-selenium-webdriver")
-    return d
